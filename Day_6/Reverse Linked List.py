3e41fb29105a0e45635bce9ff9b657b8928695cb
SrinivasGadham <gadamsrinivasyadav7@gmail.com>
2023-06-11 23:41:12 +0530

    day6commit

---
+++ Day_6/Reverse Linked List.py
@@ -0,0 +1,29 @@
+from math import *
+from collections import *
+from sys import *
+from os import *
+
+"""***************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    class Node:
+        def __init__(self, data):
+            self.data = data
+            self.next = None
+
+
+*****************************************************************"""
+
+
+def reverseLinkedList(head):
+    prev = None
+       
+    curr = head
+    
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    return prev 
